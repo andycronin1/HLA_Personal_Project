db82d8d185fcfd4c0de82ca2140330c3e3e3586a
Andy Cronin <ajcronin@deloitte.co.uk>
2026-03-31 11:53:51 +0100

    changing to c++17

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,14 @@ std::cout << "----------------------------" << std::endl;
 int main() { 
 
     // Confirm C++ version
-    long cpp_version = __cplusplus;
-
-    std::cout << "C++ Standard being used: " << cpp_version << std::endl;
+    if (__cplusplus == 202302L) std::cout << "C++23";
+    else if (__cplusplus == 202002L) std::cout << "C++20";
+    else if (__cplusplus == 201703L) std::cout << "C++17";
+    else if (__cplusplus == 201402L) std::cout << "C++14";
+    else if (__cplusplus == 201103L) std::cout << "C++11";
+    else if (__cplusplus == 199711L) std::cout << "C++98";
+    else std::cout << "pre-standard C++." << __cplusplus;
+    std::cout << "\n";
 
     std::cout << "--- Starting Car Simulation ---" << std::endl;
 
